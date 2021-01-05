c1fe6f8dd60b0e3d7ed33c6e26fc8614915fd03d
arielsaadon <55316869+arielsaadon@users.noreply.github.com>
2021-01-06 00:06:42 +0200

    #6 add shortest_path func with dijkstra algorithm

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,4 +1,6 @@
+import heapq
 import json
+import math
 from typing import List
 
 from DiGraph import DiGraph
@@ -8,7 +10,7 @@ from GraphInterface import GraphInterface
 
 class GraphAlgo(GraphAlgoInterface):
 
-    def __init__(self, di_graph: DiGraph):
+    def __init__(self, di_graph: DiGraph = DiGraph()):
         self.graph = di_graph
 
     def get_graph(self) -> GraphInterface:
@@ -49,7 +51,24 @@ class GraphAlgo(GraphAlgoInterface):
             return False
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        pass
+        nodes_list = self.graph.get_all_v()
+        if id1 is not id2 and nodes_list.get(id1) is not None:
+            self.dijkstra(id1)
+
+        path = []
+        dest_node = nodes_list.get(id2)
+        distance_path = dest_node.weight
+
+        while dest_node is not nodes_list.get(id1):
+            if dest_node is None:
+                return math.inf, []
+            path.append(dest_node.key)
+            dest_node = nodes_list.get(dest_node.father)
+
+        path.append(dest_node)
+        path.reverse()
+
+        return distance_path, path
 
     def connected_component(self, id1: int) -> list:
         pass
@@ -59,3 +78,29 @@ class GraphAlgo(GraphAlgoInterface):
 
     def plot_graph(self) -> None:
         pass
+
+    def dijkstra(self, src: int):
+        nodes_list = self.graph.get_all_v()
+        for node in nodes_list.values():
+            node.tag = False
+            node.weight = math.inf
+            node.father = None
+        start_node = nodes_list.get(src)
+        start_node.weight = 0
+
+        priority_queue = [node for node in nodes_list.values()]
+        heapq.heapify(priority_queue)
+
+        while len(priority_queue):
+            node = heapq.heappop(priority_queue)
+            neighbours = self.graph.all_out_edges_of_node(node.key)
+            if neighbours is not None and node.tag is False:
+                for neighbour in neighbours:
+                    distance = node.weight + neighbour.weight
+                    neighbour_node = nodes_list.get(neighbour.dest)
+                    if distance < neighbour_node.weight:
+                        neighbour_node.weight = distance
+                        neighbour_node.father = node.key
+                        heapq.heapify(priority_queue)
+
+            node.tag = True
--- src/NodeData.py
+++ src/NodeData.py
@@ -1,14 +1,19 @@
+import math
+
+
 class NodeData:
-    def __init__(self, key: int, pos: tuple = None, weight: int = -1, tag: int = -1, info: str = ""):
+    def __init__(self, key: int, pos: tuple = None, tag: bool = False, father: int = None):
         self.key = key
         self.tag = tag
-        self.info = info
-        self.weight = weight
+        self.father = father
+        self.weight = math.inf
         self.location = pos
+
+    def __lt__(self, other):
+        return self.weight < other.weight
 
     def __str__(self):
         return f'{self.key}'
 
     def __repr__(self):
         return self.__str__()
-
